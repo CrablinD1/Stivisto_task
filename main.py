7b4fcac782455986271098fda2e79553fb032182
CrablinD1 <64089854+CrablinD1@users.noreply.github.com>
2021-05-18 18:06:52 +0300

    sound request + amount of all received requests by types

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import emoji
+from collections import Counter
 from datetime import datetime, timedelta
+
+import emoji
 
 from dummy_server.server import get_random_request
 
@@ -40,14 +42,41 @@ def process_video(request):
         return count_extension(content[1], 3)
 
 
+def process_sound(request):
+    # Took algorithm from
+    # geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character
+
+    content = request['content']
+    freq = Counter(content)
+
+    for i in content:
+        if freq[i] == 1:
+            return i
+
+    return 'None'
+
+
 if __name__ == "__main__":
+    count = {
+        'text': 0,
+        'image': 0,
+        'video': 0,
+        'sound': 0
+    }
     for _ in range(10):
         request = get_random_request()
+        print()
         print(request)
         if request['type'] == 'text':
+            count['text'] += 1
             print(process_text(request))
         elif request['type'] == 'image':
+            count['image'] += 1
             print(process_image(request))
         elif request['type'] == 'video':
+            count['video'] += 1
             print(process_video(request))
-
+        elif request['type'] == 'sound':
+            count['sound'] += 1
+            print(process_sound(request))
+    print('\n', count)
