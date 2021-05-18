0f658abd67ad86dda0ee3869a836a773d1be73ef
CrablinD1 <64089854+CrablinD1@users.noreply.github.com>
2021-05-18 17:25:53 +0300

    text requests done

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import emoji
+
 from dummy_server.server import get_random_request
 
-# Dummy server generates random requests, 
-# your goal is to process them as per task requirements (see README.md)
+
+def process_text(request):
+    request_date = request['ts'].weekday()
+    if request_date == 5:
+        return 'Saturday: ' + emoji.emojize(":keycap_6:")
+    elif request_date == 6:
+        return 'Sunday: ' + emoji.emojize(":keycap_7:")
+    else:
+        content = request['content'].lower().split()
+        return len(set(content))
+
 
 if __name__ == "__main__":
     for _ in range(10):
         request = get_random_request()
         print(request)
-        # process request below
+        if request['type'] == 'text':
+            print(process_text(request))
